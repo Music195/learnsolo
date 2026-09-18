49d1c7d37789b2b6a0ef054b1078b8f79cc86b83
Music195 <minthetkhine195@gmail.com>
2026-09-18 15:59:23 +0900

    Rearrange the codes for Data Analysis

--- static/js/drawing_helper.js
+++ static/js/drawing_helper.js
@@ -1,4 +1,4 @@
-export function setupCanvas(canvas, aspect = 0.6) {
+function setupCanvas(canvas, aspect = 0.6) {
     if (!canvas) throw new Error("Canvas element not found");
     const ctx = canvas.getContext("2d");
     // Get the device pixel ratio
@@ -28,7 +28,7 @@ export function setupCanvas(canvas, aspect = 0.6) {
     }; //return drawing context for drawing in CSS pixels 
 }
 
-export function drawDot(ctx, x, y, radius = 10, text = null, color = "#3498db") {
+function drawDot(ctx, x, y, radius = 10, text = null, color = "#3498db") {
     ctx.save();
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
@@ -45,7 +45,7 @@ export function drawDot(ctx, x, y, radius = 10, text = null, color = "#3498db") 
     ctx.restore();
 }
 
-export function drawTitle(ctx, x, y, text, size = null, color = null) {
+function drawTitle(ctx, x, y, text, size = null, color = null) {
     ctx.save();
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
@@ -59,7 +59,7 @@ export function drawTitle(ctx, x, y, text, size = null, color = null) {
     ctx.restore();
 }
 
-export function drawDesc(ctx, x, y, text, size = 14, color = null) {
+function drawDesc(ctx, x, y, text, size = 14, color = null) {
     ctx.save();
     ctx.fillStyle = color !== null ? color : "#000000ff";
     ctx.font = `${size}px system-ui, Arial`;
@@ -69,7 +69,7 @@ export function drawDesc(ctx, x, y, text, size = 14, color = null) {
     ctx.restore();
 }
 
-export function drawArrow(ctx, x, y, fontSize = null,) {
+function drawArrow(ctx, x, y, fontSize = null,) {
     ctx.save();
     ctx.fillStyle = "#e74c3c";
     ctx.font = fontSize !== null ? fontSize + "px Arial" : "14px Arial";
@@ -78,7 +78,7 @@ export function drawArrow(ctx, x, y, fontSize = null,) {
 }
 
 //Reusalble dashed lines
-export function drawLine(ctx, x1, y1, x2, y2, dash = null, w = 1, color = "#888") {
+function drawLine(ctx, x1, y1, x2, y2, dash = null, w = 1, color = "#888") {
     ctx.save();
     if (dash !== null) {
         ctx.setLineDash(dash);
@@ -92,7 +92,7 @@ export function drawLine(ctx, x1, y1, x2, y2, dash = null, w = 1, color = "#888"
     ctx.restore();
 }
 
-export function drawBox(ctx, x, y, w, h, lw = 2) {
+function drawBox(ctx, x, y, w, h, lw = 2) {
     ctx.save();
     ctx.fillStyle = "rgba(100,160,255,0.35)";
     ctx.strokeStyle = "rgba(30,90,200,1)";
@@ -102,7 +102,7 @@ export function drawBox(ctx, x, y, w, h, lw = 2) {
     ctx.restore();
 }
 
-export function drawWhiskerLine(ctx, xBoxLeft, xDMax, xBoxRight, xDMin, BoxCenterY, capT, capB, w, color = "blue" ) {
+function drawWhiskerLine(ctx, xBoxLeft, xDMax, xBoxRight, xDMin, BoxCenterY, capT, capB, w, color = "blue" ) {
     //Right horizontal whiskers line
     drawLine(ctx, xBoxLeft, BoxCenterY, xDMax, BoxCenterY, null, w, color);
     //right cap
@@ -113,7 +113,7 @@ export function drawWhiskerLine(ctx, xBoxLeft, xDMax, xBoxRight, xDMin, BoxCente
     drawLine(ctx, xDMin, capT, xDMin, capB, null, w, color);
 };
 
-export function xScale(x, xMin, xMax, width, marginLeft, marginRight) {
+function xScale(x, xMin, xMax, width, marginLeft, marginRight) {
     const denom = xMax - xMin;
     if (denom === 0) {
         throw new Error("Maximum X coordinate must not equal minimum X coordinate.");
@@ -125,7 +125,7 @@ export function xScale(x, xMin, xMax, width, marginLeft, marginRight) {
     return marginLeft + clampedRatio * (width - (marginLeft + marginRight));
 }
 
-export function yScale(y, yMin, yMax, height, marginTop, marginBottom) {
+function yScale(y, yMin, yMax, height, marginTop, marginBottom) {
     const denom = yMax - yMin;
     if (denom === 0) {
         throw new Error("Maximum Y coordinate must not equal minimum Y coordinate.");
@@ -137,11 +137,11 @@ export function yScale(y, yMin, yMax, height, marginTop, marginBottom) {
     return height - marginBottom - clampedRatio * (height - (marginTop + marginBottom));
 }
 
-export function clear(ctx, width, height) {
+function clear(ctx, width, height) {
     ctx.clearRect(0,0, width, height);
 }
 
-export function grid(ctx, width, height, padding, lineWidth, gridWidth, color) {
+function grid(ctx, width, height, padding, lineWidth, gridWidth, color) {
     if (gridWidth <= 0) {
         console.warn("Grid width must be greater than 0 to draw grid line");
         return;
@@ -151,15 +151,7 @@ export function grid(ctx, width, height, padding, lineWidth, gridWidth, color) {
 
     ctx.strokeStyle = color;
     ctx.lineWidth = lineWidth;
-
-    //Center grid inside padding area
-    // const usableWidth = width - padding * 2;
-    // const usableHeight = height - padding * 2;
-
-    // const offsetX = padding + (usableWidth % gridWidth) / 2;
-    // const offsetY = padding + (usableHeight % gridWidth ) / 2;
-
-    
+ 
     
     //vertical lines
     for (let x = padding; x < width; x += gridWidth) {
@@ -181,7 +173,7 @@ export function grid(ctx, width, height, padding, lineWidth, gridWidth, color) {
 }
 
 // to draw the X and Y axes on the canvas
-export function axes(ctx, height, width, padding, lineWidth, color) {
+function axes(ctx, height, width, padding, lineWidth, color) {
     ctx.save();
 
     ctx.strokeStyle = color;
@@ -201,21 +193,38 @@ export function axes(ctx, height, width, padding, lineWidth, color) {
 }
 
 //to draw tick labels for the axes showing the min/max values on the X and Y axes
-export function ticks(ctx, xmin, xmax, ymin, ymax, padding, height, width, size = 11, color = "#555")  {
-    ctx.save();
+function ticks() {
+    ctx.fillStyle = "#555";
+    ctx.font = "11px system-ui";
 
-    ctx.fillStyle = color;
-    ctx.font = `${size} system-ui`;
-    ctx.fillText(xmin, padding,  height - padding + size);
-    ctx.fillText(xmax, width - padding - size, height - padding + size);
-    ctx.fillText(ymax, padding - size, padding + size);
-    ctx.fillText(ymin, padding - size, height - padding);
+    const divisions = 5;
 
-    ctx.restore();
+    for (let i = 0; i <= divisions; i++) {
 
+        const xValue = xmin + (xmax - xmin) * i / divisions;
+        const x = map(xValue, xmin, xmax, PAD, W - PAD);
+
+        ctx.fillText(
+            xValue.toFixed(1),
+            x - 8,
+            H - PAD + 15
+        );
+    }
+
+    for (let i = 0; i <= divisions; i++) {
+
+        const yValue = ymin + (ymax - ymin) * i / divisions;
+        const y = map(yValue, ymin, ymax, H - PAD, PAD);
+
+        ctx.fillText(
+            yValue.toFixed(1),
+            PAD - 30,
+            y + 4
+        );
+    }
 }
 
-export function points ({
+function points ({
         ctx,
         xMin, xMax, yMin, yMax,
         xRange, yRange,
@@ -237,7 +246,7 @@ export function points ({
     ctx.restore();
 }
 
-export function trendLine({
+function trendLine({
         ctx,
         xMin, xMax, yMin, yMax,
         xRange, yRange,
@@ -271,4 +280,203 @@ export function trendLine({
     ctx.stroke();
 
     ctx.restore();
+}
+
+
+
+//computing mean
+function mean(values) {
+    const n = values.length;
+
+    //safety check: mean is undefined for empty data;
+    if (n === 0) return NaN;
+
+    const sum = values.reduce(
+        (sum, value) => sum + value,
+        0
+    ); // add all value until 0
+    const mean = sum / n;
+
+    return mean;
+};
+
+//Reusable function
+
+// Median function 
+function median(sortedValues) {
+    const n = sortedValues.length;
+    const mid = Math.floor(n / 2); // rounding down to the nearest integer
+    return (n % 2 === 0)
+        ? (sortedValues[mid - 1] + sortedValues[mid]) / 2
+        : sortedValues[mid];
+};
+
+//computing qurtiles conceptually using median function
+function quartiles(sortedValues) {
+    const n = sortedValues.length;
+    const mid = Math.floor(n / 2);
+
+    const lower = sortedValues.slice(0, mid);
+    const upper = (n % 2 === 0) ? sortedValues.slice(mid) : sortedValues.slice(mid + 1);
+
+    return {
+        Q1: median(lower),
+        Q2: median(sortedValues),
+        Q3: median(upper)
+    };
+};
+
+
+
+//computing mode
+function mode(values) {
+    if (values.length === 0) {
+        return { frequencyMap: new Map(), maxFreq: 0, modes: [] };
+    }
+
+    const freq = new Map(); // Map store key data value pair
+
+    values.forEach(v => {
+        freq.set(v, (freq.get(v) || 0) + 1);
+    });
+
+    const maxFreq = Math.max(...freq.values());
+
+    if (maxFreq === 1) {
+        return { frequencyMap: freq, maxFreq, modes: [] };
+    };
+
+    const modes = [...freq.entries()]             //convert map to array
+        .filter(([_, count]) => count === maxFreq)    //Keep only values that appear most often
+        .map(([value]) => value)                      //Extract just the value (ignore count)
+        .sort((a, b) => a - b);
+
+    return { frequencyMap: freq, maxFreq, modes };
+
+};
+
+//computing total sum
+function totalSum(values) {
+    const totalSum = values.reduce(
+        (sum, value) => sum + value,
+        0
+    );
+    return totalSum;
+};
+
+//computing population variance
+function populationVariance(values, mean) {
+    const n = values.length;
+    // Variance is undefined for empty data
+    if (n === 0) return NaN;
+
+    const sumSquaredDeviations = values.reduce(
+        (sum, value) => sum + Math.pow(value - mean, 2),
+        0
+    );
+    // Population variance = average of squared deviations
+    return sumSquaredDeviations / n;
+};
+
+//computing sample variance
+function sampleVariance(values, mean) {
+    const n = values.length;
+    if (n < 2) return NaN;
+
+    const sumSquaredDeviations = values.reduce(
+        (sum, value) => sum + (value - mean) ** 2,
+        0
+    );
+
+    return sumSquaredDeviations / (n - 1);
+}
+
+//computing correlation
+function correlation (x, y) {
+    const mx = mean(x), my = mean(y);
+    let sxy = 0, sx = 0, sy = 0;
+
+    for (let i = 0; i < x.length; i++) {
+        sxy += (x[i] - mx) * (y[i] - my);
+        sx += (x[i] - mx) ** 2;
+        sy += (y[i] - my) ** 2;
+    }
+
+    let strength;
+
+    if (sx === 0 || sy === 0) {
+        return { strength: "undefined", r: NaN };
+    }
+
+    const r = sxy / Math.sqrt(sx * sy);
+    const a = Math.abs(r);
+
+    if (a > 0.8) strength = "Strong";
+    else if (a > 0.5) strength = "Moderate";
+    else if (a > 0.3) strength = "Weak";
+    else strength = "Very weak or None";
+
+    return {strength, r };
+}
+
+// Checking string
+function readStr(id, allowed = null) {
+    const el = document.getElementById(id);
+    if (!el) throw new Error(`${id} not found`);
+
+    const value = String(el.value).trim().toLowerCase();
+    if (!value) throw new Error(`${id} is empty`);
+
+    if (allowed && !allowed.includes(value)) {
+        throw new Error(`${id} must be one of: ${allowed.join(", ")}`);
+    }
+
+    return value;
+}
+
+// Checking numeric array 
+function readNumericArray(id) {
+    const el = document.getElementById(id);
+    if (!el) throw new Error(`${id} not found`);
+    if (!el.value.trim()) throw new Error(`${id} is empty`);
+
+    const arr = el.value
+        .split(",")
+        .map(v => Number(v.trim()))
+        .filter(v => !Number.isNaN(v));
+
+    if (arr.length ===0) {
+        throw new Error(`${id} contains no valid numbers`);
+    }
+
+    return arr;
+}
+
+// Checking div element
+function readDivElement (id) {
+    const el = document.getElementById(id);
+    if(!el) throw new Error (`${id} not found`);
+    return el;
+}
+
+//to get the maximum and minimum value of x and y 
+function getBoundaryValue (x, y, paddingRatio = 0.05) {
+    if (!x.length || !y.length) {
+        throw new Error("Array must not be empty");
+    }
+
+    const xmin = Math.min(...x);
+    const xmax = Math.max(...x);
+    const ymin = Math.min(...y);
+    const ymax = Math.max(...y);
+
+    const xRange = xmax - xmin;
+    const yRange = ymax - ymin;
+
+    return {
+        xmin: xmin - xRange * paddingRatio,
+        xmax: xmax - xRange * paddingRatio,
+        ymin: ymin - yRange * paddingRatio,
+        ymax: ymax - yRange * paddingRatio
+    };
 }
